9232e0b8ab534bbfb478e9c27bcf54fe1569075b
Ritik Gupta <38307256+ritikgi@users.noreply.github.com>
2022-10-18 23:43:17 +0530

    Added solution - LeetHub

---
+++ Missing number in array - GFG/missing-number-in-array.java
@@ -0,0 +1,59 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+
+        int t = Integer.parseInt(br.readLine().trim());
+        while (t-- > 0) {
+            int n = Integer.parseInt(br.readLine().trim());
+            String[] str = br.readLine().trim().split(" ");
+            int[] array = new int[n - 1];
+            for (int i = 0; i < n - 1; i++) {
+                array[i] = Integer.parseInt(str[i]);
+            }
+            Solution sln = new Solution();
+            System.out.println(sln.MissingNumber(array, n));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    int MissingNumber(int arr[], int n) {
+        // Sort and search
+        // Arrays.sort(arr);
+        // int missing=0;
+        // for(int i =0;i<arr.length;i++)
+        // {
+        //     if(arr[i] != i+1)
+        //     {
+        //         missing= i+1;
+        //     }
+        // }
+        // return missing;
+        
+        //Better Approach HashMap method we can use 
+         
+        
+        //best approach --> sum of natural number
+        int realSum = n*(n+1)/2;
+        int currSum = 0;
+        for(int i =0; i<arr.length;i++)
+        {
+            currSum += arr[i];
+        }
+        return realSum-currSum;
+        
+        
+        
+    }
+}
